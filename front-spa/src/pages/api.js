196d1d5e0e5f5d85d1bdd215017b660295d3f318
Lutnik <hubert.mazurkiewicz@gmail.com>
2020-09-03 14:25:19 +0200

    Update API page

--- front-spa/src/pages/api.js
+++ front-spa/src/pages/api.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const API = () => {
-  const appUrl = 'https://lorem-silesium.herokuapp.com/';
+  const appUrl = 'https://lorem-slunskum.pl/';
   const codeStyle = {
     fontWeight: 600,
     background: 'rgba(255, 255, 255, 0.6)',
@@ -25,17 +25,11 @@ const API = () => {
       <SEO title="Detale API" />
       <h2 style={{ marginBottom: 0 }}>API</h2>
       <section>
-        <p>
-          Chcesz skorzystać z otwartego API? Nic prostszego!
-          Pobieraj dane z poniższego adresu:
-        </p>
-        <code style={codeStyle}>
-          {`${appUrl}api/paragraphs/XXX/words/YYY`}
-        </code>
+        <p>Chcesz skorzystać z otwartego API? Nic prostszego! Pobieraj dane z poniższego adresu:</p>
+        <code style={codeStyle}>{`${appUrl}api/paragraphs/XXX/words/YYY`}</code>
         <br />
         <span style={{ fontSize: 'small' }}>
-          (XXX to liczba paragrafów (1 - 5),
-          a YYY to liczba słów w każdym z nich (1 - 100))
+          (XXX to liczba paragrafów (1 - 5), a YYY to liczba słów w każdym z nich (1 - 100))
         </span>
         <p>
           API wygeneruje za każdym razem losowy tekst z bazy danych ponad 10000 słów.
@@ -55,7 +49,7 @@ const API = () => {
           &nbsp;&nbsp;.then(res =&gt; console.log(res))
         </code>
         <p> Dostarczy odpowiedź: </p>
-        <code style={codeStyle}>  Object &#123; status, error, body[] &#125; </code>
+        <code style={codeStyle}> Object &#123; status, error, body[] &#125; </code>
         <p>
           <span style={spanStyle}>status</span>
           to 200, jeśli udało się pobrać dane, lub 4XX, jeśli wystąpił problem.
